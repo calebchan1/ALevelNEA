38d6a29bec31cd585dd3efab78951d25c1548c37
Caleb Chan <calebchanwy@gmail.com>
2021-09-02 12:08:07 +0100

    02/09/21 - Gyroscope Spiking

--- spiking/app/src/main/java/com/example/gyroscopespiking/MainActivity.java
+++ spiking/app/src/main/java/com/example/gyroscopespiking/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager sensorManager;
     private Sensor gyroscope;
     private SensorEventListener gyroscopeEventListener;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
+        textView = findViewById(R.id.textView);
         if (gyroscope == null) {
             Toast.makeText(this, "This device has no gyroscope", Toast.LENGTH_SHORT).show();
             finish();
@@ -30,29 +32,32 @@ public class MainActivity extends AppCompatActivity {
         gyroscopeEventListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if (event.values[2]>0.5f){
+                if (event.values[2]>1f){
+                    float value = event.values[2];
+                    String s = Float.toString(value);
+                    textView.setText(s);
                     getWindow().getDecorView().setBackgroundColor(Color.BLUE);
                 }
-                else if (event.values[2]<-0.5f){
+                else if (event.values[2]<-1f){
                     getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
                 }
             }
-
             @Override
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
             }
         };
     }
 
     @Override
     protected void onResume() {
+        //when the user resumes the app
         super.onResume();
         sensorManager.registerListener(gyroscopeEventListener,gyroscope,SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
     protected void onPause() {
+
         super.onPause();
         sensorManager.unregisterListener(gyroscopeEventListener);
     }
