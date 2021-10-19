31a006eba9affa41509043a76e9b947ad02211f5
Caleb Chan <calebchanwy@gmail.com>
2021-10-19 22:25:18 +0100

    19/09/21

--- ExerciseTracker/app/src/main/java/com/example/exercisetracker/ExerciseFragment.java
+++ ExerciseTracker/app/src/main/java/com/example/exercisetracker/ExerciseFragment.java
@@ -10,5 +10,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ExerciseFragment extends Fragment {
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_exercise,container,false);
+    }
 }
--- ExerciseTracker/app/src/main/java/com/example/exercisetracker/MainActivity.java
+++ ExerciseTracker/app/src/main/java/com/example/exercisetracker/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.exercisetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity{
     private BottomNavigationView bottomMenu;
@@ -16,8 +20,23 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
+        bottomMenu.findViewById(R.id.bottom_navigation);
+        bottomMenu.setSelectedItemId(R.id.page_1);
+        bottomMenu.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch(item.getItemId()){
+                    case(R.id.page_3):
+                        fragment = new ExerciseFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+                return true;
+            }
+        });
 
     }
+
 
 }
