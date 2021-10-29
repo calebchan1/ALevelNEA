46cea59189b32f3601d2e1731f938b32aeadfcac
Caleb Chan <calebchanwy@gmail.com>
2021-10-29 17:13:27 +0100

    29/10/21 RunningTimers

--- ExerciseTracker/app/src/main/java/com/example/exercisetracker/RunningActivity.java
+++ ExerciseTracker/app/src/main/java/com/example/exercisetracker/RunningActivity.java
@@ -1,48 +1,142 @@
 package com.example.exercisetracker;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Timer;
+
 public class RunningActivity extends AppCompatActivity {
     private SensorManager sensorManager;
     private Sensor accelerometer;
     private SensorEventListener accelerometerEventListener;
+    private TextView timerText;
+    private TextView stepText;
+    private TextView calorieText;
+    private TextView distText;
+    private MaterialButton finishBtn;
+    private MaterialButton startStopBtn;
+    private Boolean isRunning;
+    private Integer seconds;
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_running);
+        //instantiating all private variables
+        isRunning = true;
+        seconds = 0;
+        timerText = findViewById(R.id.timerText);
+        stepText = findViewById(R.id.stepText);
+        distText = findViewById(R.id.distText);
+        calorieText = findViewById(R.id.calText);
         sensorManager =(SensorManager)getSystemService(SENSOR_SERVICE);
-        accelerometer =sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        startStopBtn = findViewById(R.id.startStopBtn);
+        startStopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isRunning){
+                    isRunning = false;
+                }
+                else{
+                    isRunning = true;
+                }
+            }
+        });
+
+        //creating handler to run simultaneously to track duration in seconds
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time
+                        = String
+                        .format(Locale.getDefault(),
+                                "%d:%02d:%02d", hours,
+                                minutes, secs);
+                timerText.setText(time);
+                if (isRunning){
+                    seconds ++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+
+        finishBtn = findViewById(R.id.finishBtn);
+        finishBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //exiting the running activity and sending data back to main program
+                finish();
+            }
+        });
+
+        //handling accelerometer permissions
         checkPermissions();
         if (accelerometer==null){
             Toast.makeText(this, "No accelerometer detected", Toast.LENGTH_SHORT).show();
             finish();
         }
+
+        //handling accelerometer
+        accelerometerEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                DecimalFormat df = new DecimalFormat("#.####");
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                float mag = (float) Math.sqrt(x*x + y*y + z*z);
+                stepText.setText("Steps: \n" +mag);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            }
+        };
+        sensorManager.registerListener(accelerometerEventListener,accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void checkPermissions() {
         int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
             return;
         }
         Toast.makeText(getBaseContext(), "Permission is already granted", Toast.LENGTH_LONG).show();
     }
+
+
 
 
 }
