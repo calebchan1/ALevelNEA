c455b575e281f3fd79c0eb6ce0afbc7910dc80a8
Caleb Chan <calebchanwy@gmail.com>
2022-02-21 21:48:27 +0000

    21/02/22 1.1-3.1 Testin Videos

--- PHP_Scripts/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-    //setting all constants, which correspond with the database
-    define('dbname','sql4456768');
-    define('dbuser','sql4456768');
-    define('dbpassword','gyFr8LHqQA');
-    define('dbhost','sql4.freesqldatabase.com');
